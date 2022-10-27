3f05dc9edc1fa3b6fc8f9f0e55386596888810fd
Ingrid <115805965+Triangi3@users.noreply.github.com>
2022-10-26 23:46:08 -0300

    Add files via upload

---
+++ lojinha.js
@@ -0,0 +1,115 @@
+const Items =[
+    {
+     id: 0,
+     nome: "anel0",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 1,
+     nome: "anel1",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 2,
+     nome: "anel2",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 3,
+     nome: "anel3",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 4,
+     nome: "pingente1",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 5,
+     nome: "pingente2",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 6,
+     nome: "pingente3",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 7,
+     nome: "pingente4",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 8,
+     nome: "brinco1",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 9,
+     nome: "brinco2",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 10,
+     nome: "brinco3",
+     img: "images.jpg",
+     quantidade: 0
+    },
+    {
+     id: 11,
+     nome: "brinco4",
+     img: "images.jpg",
+     quantidade: 0
+    },
+
+ ]
+     
+ function inicializarLoja(){
+ 
+var   containerProduto = document.getElementById("Produtos");
+Items.map((val)=>{
+  containerProduto.innerHTML+= `
+
+  <div class= "Produto-single"> 
+      <img src="`+val.img+`" />
+      <p>`+val.nome+`</p>
+      <a key="`+val.id+`" href="#">Adicionar ao carrinho!<a/>
+
+  </div>
+
+  `
+})
+  }
+  inicializarLoja();
+
+  atualizarCarrinho = () => {
+    console.log(Items);
+}
+
+var links = document.getElementsByTagName("a");
+   for(var i = 0; i < links.length; i++){
+   links[i].addEventListener("click",function(){
+      let key = this.getAttribute("key");
+      Items[key].quantidade++;
+      atualizarCarrinho();
+      return false;
+   })
+   }
+
+
+
+
+
+
+
+
